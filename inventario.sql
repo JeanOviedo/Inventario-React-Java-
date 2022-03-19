414b6fd531cf3844a6e031d7e17bc37963f909c4
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2022-03-19 05:06:19 -0500

    Update...

--- inventario.sql
+++ inventario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-03-2022 a las 10:41:04
+-- Tiempo de generación: 19-03-2022 a las 11:01:01
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `productos` (
   `id` bigint(20) NOT NULL,
   `categoria` varchar(255) DEFAULT NULL,
-  `date` varchar(255) NOT NULL DEFAULT current_timestamp(),
+  `date` varchar(255) NOT NULL,
   `nombre` varchar(255) DEFAULT NULL,
   `peso` int(11) DEFAULT NULL,
   `precio` int(11) DEFAULT NULL,
@@ -44,14 +44,15 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `categoria`, `date`, `nombre`, `peso`, `precio`, `referencia`, `stock`, `ventas`) VALUES
-(6, 'ddd', '', 'Yuca', 23, 3000, 'dwewew', 8, 4),
 (7, 'test', '', 'Papa', 8878, 7767, 'asasas', 0, 1),
-(8, 'Prueba', '', 'Pepinillo', 3232, 712323, 'wewewe', 9, 7),
-(12, 'Categoria3', '', 'Ajo', 345, 232, 'ererer', 18, 0),
+(8, 'Prueba', '2022-03-19 09:59:26', 'Pepinillo', 3232, 712323, 'wewewe', 7, 9),
+(12, 'Categoria3', '2022-03-19 09:59:49', 'Ajo', 345, 232, 'ererer', 17, 1),
 (14, 'Cate45', '', 'Yogur', 232, 1212, 'wrererere', 119, 2),
 (16, 'wewe', '', 'Leche', 233, 2323, '32323', 2318, 5),
 (17, 'sdswe', '', 'Perenjil', 343, 323, 'rererer', 2313, 0),
-(19, 'ewe', '', 'Wfer', 3232, 232, 'wewewe', 0, 3);
+(19, 'ewe', '', 'Wfer', 3232, 232, 'wewewe', 0, 3),
+(21, 'ewew', '2022-03-19 09:55:44', 'Tequila', 3232, 34, '2323', 23, 0),
+(22, 'ere', '2022-03-19 09:58:48', 'Galleta', 434, 343, '3434', 323, 0);
 
 -- --------------------------------------------------------
 
@@ -94,7 +95,7 @@ ALTER TABLE `ventas`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
